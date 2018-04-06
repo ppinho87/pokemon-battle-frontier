290eb462bed95d800010d0b8b6212f409ee7aa6b
Paul <pinhop07@gmail.com>
2018-04-06 16:17:28 -0400

    updates

--- app/client/components/modules/common/copyright.jsx
+++ app/client/components/modules/common/copyright.jsx
@@ -3,9 +3,9 @@ App.Copyright = React.createClass({
         return (
             <module className="copyright module">
                 <p className="copyright">
-                    &copy;2018, <a className="creator" href="http://damirvazgird.com"
+                    &copy;2018, <a className="creator" href="http://interactive-resume-191115.appspot.com"
                                    target="_blank">Paul Pinho</a>. Code available on <a
-                    className="creator" href="https://github.com/pinhop07/pokemon-battleship"
+                    className="creator" href="https://github.com/pinhop07/pokemon-battle-frontier"
                     target="_blank">Github</a>.
                 </p>
             </module>
--- app/client/components/modules/common/logo.jsx
+++ app/client/components/modules/common/logo.jsx
@@ -2,7 +2,7 @@ App.Logo = React.createClass({
     render() {
         return (
             <module className="animated fadeInDown logo module">
-                <a className="logo" href={RouterHelpers.pathFor('root')}>Pokemon <i className="fa fa-dot-circle-o"></i> Battle</a>
+                <a className="logo" href={RouterHelpers.pathFor('root')}>Pokemon <i className="fa fa-dot-circle-o"></i> Battle Frontier</a>
             </module>
         );
     }
